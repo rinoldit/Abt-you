afe592a2668351b26464bd449d2f2353b64d7c16
Rinold <rinoldit@gmail.com>
2017-08-30 00:10:12 +0530

    Created Basic Info component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BasicInfoComponent } from './basic-info/basic-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicInfoComponent
   ],
   imports: [
     BrowserModule,
